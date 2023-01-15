a3e679d072fa8739be062ae6f01ddf54e2401ab0
Saugat Adhikari <saugat@mediprocomputers.com>
2023-01-15 17:22:01 +0545

    service page child routes added

--- src/app/content/pages/services/services.module.ts
+++ src/app/content/pages/services/services.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesComponent } from './services.component';
 import {MatStepperModule} from '@angular/material/stepper';
+import { RouterModule } from '@angular/router';
+import { serviceRoutes } from 'src/app/routing/service-routes';
 
 
 @NgModule({
   declarations: [
-    ServicesComponent
+    ServicesComponent,
   ],
   imports: [
     CommonModule,
     MatStepperModule,
+    RouterModule.forChild(serviceRoutes),
+
   ]
 })
 export class ServicesModule { }
