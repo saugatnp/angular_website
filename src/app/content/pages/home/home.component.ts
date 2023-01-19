35caac2d272fff3f40101151d8e1973f999ac5c6
Saugat Adhikari <saugat@mediprocomputers.com>
2023-01-19 16:07:58 +0545

    cancer care updated

--- src/app/content/pages/home/home.component.ts
+++ src/app/content/pages/home/home.component.ts
@@ -18,37 +18,37 @@ export class HomeComponent implements OnInit {
   service = [
     {
       name: 'Medical Department',
-      image: 'https://www.nch.com.np/wp-content/uploads/2020/06/Medical-Oncologist-2-768x369.jpg',
+      image: 'https://scontent.fbwa1-1.fna.fbcdn.net/v/t39.30808-6/277674963_3247016085621814_6156537813936942794_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=9dOHyYJAL68AX9BQIZL&_nc_ht=scontent.fbwa1-1.fna&oh=00_AfDlmjtSbjOmXIWHzk2HeKJ6ice7ayZbkSKeU7iCoT1kfg&oe=63CE9762',
       text: 'Department of Medical Oncology at Nepal Cancer Hospital & Research Centre Pvt. Ltd covers a very varied clinical spectrum and offeres the highest quality and advanced oncology care in a supportive and compassionate environment to all our patients for the treatment and prevention of cancers through the protocal-based approach and onnovative reseach.',
       link: ''
     },
     {
       name : 'Surgical Department',
-      image : 'https://www.nch.com.np/wp-content/plugins/js_composer/assets/vc/no_image.png',
+      image : 'https://scontent.fbwa1-1.fna.fbcdn.net/v/t39.30808-6/270774588_3182864935370263_6227456734189255812_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=db41JLNRreIAX8-WXwM&_nc_ht=scontent.fbwa1-1.fna&oh=00_AfB5qGt4osYpxq4UsOtSLI4ubJagMqxDesL5ilaeRVd9jQ&oe=63CDC5DC',
       text : 'Surgery is a medical specialty that uses operative manual and instrumental techniques on a patient to investigate or treat a pathological condition such as a disease or injury, to help improve bodily function or appearance or to repair unwanted ruptured areas.',
       link : ''
     },
     {
       name : 'Radiotherapy Deparment',
-      image : 'https://www.nch.com.np/wp-content/uploads/2020/06/DSC_3880-768x354.jpg',
+      image : 'https://scontent.fbwa1-1.fna.fbcdn.net/v/t39.30808-6/269977273_3182864708703619_2963793404533299723_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=my89RMYFLJQAX_YNaY-&_nc_ht=scontent.fbwa1-1.fna&oh=00_AfCEE6s_Ji9mUtIpMB448E44NceX3Q_gl0zaM_-1muamdQ&oe=63CDDF29',
       text : 'Radiation therapy or radiotherapy, often abbreviated RT, RTx, or XRT, is therapy using ionizing radiation, generally as part of cancer treatment to control or kill malignant cells and normally delivered by a linear accelerator.',
       link : ''
     },
     {
       name : 'Pathology',
-      image : 'https://www.nch.com.np/wp-content/uploads/2020/06/Pathology-and-Laboratory-Science-768x474.jpg',
+      image : 'https://scontent.fbwa1-1.fna.fbcdn.net/v/t31.18172-8/22859713_1944000375923398_3617661858024490318_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=5wzh6mOV4O8AX-01f2k&_nc_ht=scontent.fbwa1-1.fna&oh=00_AfDwOmjORQGvx1qZfJDJdbnFwIk66wTer5bYufSjWUbnnw&oe=63F074B0',
       text : 'Pathology is the study of the processes underlying diseases & other forms of illness, harmful abnormality, or dysfunction. Within biology but also a branch of medicine, it means specifically the study & diagnosis of the structural with functional changes in cells, tissues & organs that underlie diseases.',
       link : ''
     },
     {
       name : 'Radiology Department',
-      image : 'https://www.nch.com.np/wp-content/uploads/2020/06/Radiology-855x570.jpg',
+      image : 'https://scontent.fbwa1-1.fna.fbcdn.net/v/t31.18172-8/21457384_1917472075242895_907798514835343020_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=8fK_gKNoNtoAX_C8MSz&_nc_ht=scontent.fbwa1-1.fna&oh=00_AfCDLDp6slHIZoIwDy3n8_rg1zfe7S9w-mEpBZaLVqiT9w&oe=63F099D1',
       text : 'Radiology is the medical specialty that uses medical imaging to diagnose and treat diseases within the bodies of both humans and animals.',
       link : ''
     },
     {
       name : 'Administrative Deparment',
-      image : 'https://www.nch.com.np/wp-content/plugins/js_composer/assets/vc/no_image.png',
+      image : 'https://scontent.fbwa1-1.fna.fbcdn.net/v/t39.30808-6/270774588_3182864935370263_6227456734189255812_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=db41JLNRreIAX8-WXwM&_nc_ht=scontent.fbwa1-1.fna&oh=00_AfB5qGt4osYpxq4UsOtSLI4ubJagMqxDesL5ilaeRVd9jQ&oe=63CDC5DC',
       text : 'Governance of the NCHRC is headed by Executive Chairman. Medical Director and Chief Executive Officer are leaders of clinical and non-clinical divisions respectively. Hospital/General Administration of the NCHRC is led by Chief Executive Officer. The hospital administration is composed of several units with incharges. They are Finance, Human Resources (HR), Administration, Sales and Business Promotion (SBP), Procurement, General Store (GS), Information Technology (IT), Hospital Operations (HO), Repair, Maintenance & Engineering (RME), Biomedical Engineering (BME) and other support service units viz. Housekeeping and Laundry, Medical Records (MR), Transportation, Legal, Public Relations (PR), Helpdesk/Reception, Cash Unit, Charity Unit (Bipanna Nagarik Ausadhi Upachar Kosh), Hospital Waste Management (HWM), Food Services, Security and many more. Quality Department (QD) is directed by Executive Chairman. All incharges of the units are certified internal quality auditors (ISO 9001:2015). Everyday 60+ employees passionately work under Hospital Administration Department to achieve goal of the organization.A pioneer in world class health care system, Nepal Cancer Hospital strives to put well experienced and dedicated Administrative staffs to run comprehensive health care system.',
       link : ''
     }
